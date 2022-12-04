5c3dbd59c0dcccc119cedb7393e8591b06932dbb
K0dax <yancarlosbrg@outlook.com>
2022-12-04 19:57:20 -0300

    Input-useState

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { MdSearch } from 'react-icons/md';
 
@@ -6,17 +6,24 @@ import { Container, Logo, Title, Form, Input, Button } from './styles';
 
 import GithubLogo from '../../assets/images/github-logo.svg';
 
-const MainPage = () => (
-  <Container>
-    <Logo src={GithubLogo} alt="Github logo" />
-    <Title>API Github</Title>
-    <Form>
-      <Input placeholder="usuário" />
-      <Button>
-        <MdSearch size={42} />
-      </Button>
-    </Form>
-  </Container>
-);
+const MainPage = () => {
+  const [login, setlogin] = useState('');
+  return (
+    <Container>
+      <Logo src={GithubLogo} alt="Github logo" />
+      <Title>API Github</Title>
+      <Form>
+        <Input
+          placeholder="usuário"
+          value={login}
+          onchange={(e) => setlogin(e.target.value)}
+        />
+        <Button>
+          <MdSearch size={42} />
+        </Button>
+      </Form>
+    </Container>
+  );
+};
 
 export default MainPage;
