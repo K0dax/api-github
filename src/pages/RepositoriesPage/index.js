cd6758998314cf6a1ffab478d5c5254d4fa4bb5b
K0dax <yancarlosbrg@outlook.com>
2022-12-06 19:42:39 -0300

    RepositoriesPage base filter

--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Profile from './Profile';
 
+import Filter from './Filter';
+
 import { Container, Sidebar, Main } from './styles';
 
 const RepositoriesPage = () => (
   <Container>
     <Sidebar>
       <Profile />
+      <Filter />
     </Sidebar>
     <Main>Main</Main>
   </Container>
