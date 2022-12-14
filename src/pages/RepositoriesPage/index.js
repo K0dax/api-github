30d2d5ebfcf63c10023283cdfd6fa343907abd4b
K0dax <yancarlosbrg@outlook.com>
2022-12-14 17:53:51 -0300

    add languages service

--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -7,6 +7,7 @@ import Filter from './Filter';
 import Repositories from './Repositories';
 
 import { Container, Sidebar, Main } from './styles';
+import { getLangsFrom } from '../../services/getlangs';
 
 const RepositoriesPage = () => {
   const user = {
@@ -20,11 +21,46 @@ const RepositoriesPage = () => {
     location: 'RJ',
   };
 
+  const repositories = [
+    {
+      name: 'Repo 1',
+      desc: 'desc 1',
+      html_url: 'https://koda.com',
+      language: 'JavaScript',
+    },
+    {
+      name: 'Repo 2',
+      desc: 'desc 2',
+      html_url: 'https://koda.com',
+      language: 'PHP',
+    },
+    {
+      name: 'Repo 3',
+      desc: 'desc 3',
+      html_url: 'https://koda.com',
+      language: 'Ruby',
+    },
+    {
+      name: 'Repo 4',
+      desc: 'desc 4',
+      html_url: 'https://koda.com',
+      language: 'Python',
+    },
+    {
+      name: 'Repo 5',
+      desc: 'desc 5',
+      html_url: 'https://koda.com',
+      language: 'JavaScript',
+    },
+  ];
+
+  const languages = getLangsFrom(repositories);
+
   return (
     <Container>
       <Sidebar>
         <Profile user={user} />
-        <Filter />
+        <Filter languages={languages} />
       </Sidebar>
       <Main>
         <Repositories />
