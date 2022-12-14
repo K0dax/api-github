9d6a2e4297d96b77d2c8c9dd060eb03f8965fce5
K0dax <yancarlosbrg@outlook.com>
2022-12-13 21:46:43 -0300

    fix profile component

--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -8,16 +8,29 @@ import Repositories from './Repositories';
 
 import { Container, Sidebar, Main } from './styles';
 
-const RepositoriesPage = () => (
-  <Container>
-    <Sidebar>
-      <Profile />
-      <Filter />
-    </Sidebar>
-    <Main>
-      <Repositories />
-    </Main>
-  </Container>
-);
+const RepositoriesPage = () => {
+  const user = {
+    login: 'K0dax',
+    name: 'Yan Carlos',
+    avatar_url: 'https://avatars.githubusercontent.com/u/78356819?v=4',
+    followers: 0,
+    following: 0,
+    company: null,
+    blog: 'https://K0dax.com',
+    location: 'RJ',
+  };
+
+  return (
+    <Container>
+      <Sidebar>
+        <Profile user={user} />
+        <Filter />
+      </Sidebar>
+      <Main>
+        <Repositories />
+      </Main>
+    </Container>
+  );
+};
 
 export default RepositoriesPage;
